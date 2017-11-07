3df8708d3f13a7febc975b337a72095fdb419cfb
zzbslayer <zzbslayer@sjtu.edu.cn>
2017-11-07 16:35:57 +0800

    Simple Refactor Done

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :article
-  validates :body, presence: true,
-                    length: { minimum: 5 }
+  #validates :body, presence: true,
+  #                  length: { minimum: 5 }
 end
